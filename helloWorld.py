c7a5cbd1d5f9b26ae9fc308ecfaf4ecbd6e1ba32
Anzeela Kabir <anzeelakabir29@gmail.com>
2024-03-10 17:18:47 +0000

    Modified helloWorld.py to accept user input

--- helloWorld.py
+++ helloWorld.py
@@ -1,7 +1,20 @@
+# Practical task 1
 # helloWorld.py
 
+# Defining the main function.
 def main():
+	# Printing out the message.
     print("Git is awesome!")
 
+# Executing the main function if the script is running directly.
 if __name__ == "__main__":
     main()
+
+# Practical task 2 
+
+# This line of code prompts the user to enter their name.
+user_input = input("Enter your name:")
+
+#This line of code prints out the entered data.
+print("Hello,", user_input)
+
